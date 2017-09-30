4faa14488cf699fb6435cb0b6479f1b5d76da0c2
mr.akik <mr.akik@yahoo.com>
2017-09-30 06:33:02 +0600

    edited

--- app/urls.py
+++ app/urls.py
@@ -3,8 +3,8 @@ from app.views import index,me,job
 #from app import views
 
 urlpatterns = patterns('',
-    url(r'^$', index, name='index'),
-    url(r'^me/$', me, name='me'),
+    url(r'^$', index),
+    url(r'^me/$', me),
     url(r'^job/$', job),
-    url(r'^mother/$', mother, name='mother'),    
+    url(r'^mother/$', mother),    
 )
