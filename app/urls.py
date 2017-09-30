ef70b1f313785eb4efc4e4461fedddb5e55738b0
mr.akik <mr.akik@yahoo.com>
2017-09-30 06:26:35 +0600

    edited

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, url
-from app.views import index,me
+from app.views import index,me,job
 #from app import views
 
 urlpatterns = patterns('',
     url(r'^$', index, name='index'),
     url(r'^me/$', me, name='me'),
-    url(r'^job/$', job, name='job'),
+    url(r'^job/$', job),
     url(r'^mother/$', mother, name='mother'),    
 )
